1e80748484013b242a12f386184c369644ea6479
Casey Platt <kse0824@gmail.com>
2016-11-21 12:39:43 -0600

    review112116

--- do-nothing-app/config/routes.rb
+++ do-nothing-app/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/shows' => 'shows#index'
+  get '/shows/new' => 'shows#new'
+  post '/shows' => 'shows#create'
+  get '/shows/:id' => 'shows#show'
+  get '/shows/:id/edit' => 'shows#edit'
+  patch '/shows/:id' => 'shows#update'
+  delete '/shows/:id' => 'shows#destroy'
 end
