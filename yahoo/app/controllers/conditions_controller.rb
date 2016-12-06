1e513beb354dfaea8d1cc5db5489fdda2e147576
Casey Platt <kse0824@gmail.com>
2016-12-06 10:47:58 -0600

    finial

--- yahoo/app/controllers/conditions_controller.rb
+++ yahoo/app/controllers/conditions_controller.rb
@@ -1,7 +1,9 @@
 class ConditionsController < ApplicationController
   def index
     @conditions = Unirest.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22chicago%2C%20il%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").body
+    @temperature = @conditions['query']['results']['channel']['item']['condition']['temp']
     @forcasts = @conditions['query']['results']['channel']['item']['forecast']
+    @description = @conditions['query']['results']['channel']['item']['description']
     render 'index.html.erb'
   end
 end
