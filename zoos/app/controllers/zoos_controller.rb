55ec47e232bf4785d0b0a429b79f811d39455426
Casey Platt <kse0824@gmail.com>
2016-12-05 18:39:23 -0600

    unriest

--- zoos/app/controllers/zoos_controller.rb
+++ zoos/app/controllers/zoos_controller.rb
@@ -1,7 +1,30 @@
 class ZoosController < ApplicationController
   def index
-    @zoos=Zoo.all
+    @zoos = Zoo.all
     render 'index.html.erb'
   end
-
+  
+  def new
+    render 'new.html.erb'
+  end
+  
+  def create
+    render 'create.html.erb'
+  end
+  
+  def show
+    render 'show.html.erb'
+  end
+  
+  def edit
+    render 'edit.html.erb'
+  end
+  
+  def update
+    render 'update.html.erb'
+  end
+  
+  def destroy
+    render 'destroy.html.erb'
+  end
 end
--- zoos/config/routes.rb
+++ zoos/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/zoos' => 'zoos#index'
+  get 'zoos/new' => 'zoos#new'
+  post '/zoos' => 'zoos#create'
+  get '/zoos/:id' => 'zoos#show'
+  get '/zoos/:id/edit' => 'zoos#edit'
+  patch '/zoos/:id' => 'zoos#update'
+  delete '/zoos/:id' => 'zoos#destroy' 
 end
