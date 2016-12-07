b4e13ba1655afcb002071cbb3b426b7ad7368d10
Casey Platt <kse0824@gmail.com>
2016-12-06 20:40:36 -0600

    shoes

--- shoes/app/controllers/shoes_controller.rb
+++ shoes/app/controllers/shoes_controller.rb
@@ -9,4 +9,26 @@ class ShoesController < ApplicationController
     @shoe = Shoe.find_by(id: params[:id])
     render 'show.json.jbuilder'
   end
+
+  def create
+    @shoe = Shoe.new(
+      use: params[:use],
+      material: params[:material]
+    )
+    @shoe.save
+    # redirect_to "/employees/#{@employee.id}"
+    render 'show.json.jbuilder'
+  end
+
+
+  def update
+    @shoe = Shoe.find_by(id: params[:id])
+    @shoe.update(use: params[:use], material: params[:material])
+    render 'show.json.jbuilder' 
+  end
+
+  def destroy
+    @shoe = Shoe.find_by(id: params[:id])
+    @show.destroy
+  end
 end
--- shoes/config/routes.rb
+++ shoes/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/shoes" => 'shoes#index'
+  post '/shoes' => 'shoes#create'
   get '/shoes/:id' => 'shoes#show' 
+  patch '/shoes/:id' => 'shoes#update'
+  delete '/shoes/:id' => 'shoes#destroy'
 end
+
