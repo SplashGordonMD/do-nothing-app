a34b57adc744219ff7af0e19844d3222eec23d64
Casey Platt <kse0824@gmail.com>
2016-12-12 13:07:11 -0600

    lunch time

--- pest-app/app/controllers/bugs_controller.rb
+++ pest-app/app/controllers/bugs_controller.rb
@@ -9,18 +9,26 @@ class BugsController < ApplicationController
   end
 
   def create
+    bug = Bug.new(name: params[:name], body: params[:body])
+    bug.save
     render 'create.html.erb'
   end
 
   def show
+    @bug = Bug.find_by(id: params[:id])
     render 'show.html.erb'
   end
 
   def edit
+    @bug = Bug.find_by(id: params[:id])
     render 'edit.html.erb'
   end
 
   def update
+    @bug = Bug.find_by(id: params[:id])
+    @bug.name = params[:name]
+    @bug.body = params[:season]
+    @bug.save
     render 'update.html.erb'
   end
 
