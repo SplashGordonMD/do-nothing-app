dd6de44fed4027f5a65315bcbc3f8c562fdbcb61
Casey Platt <kse0824@gmail.com>
2016-12-19 19:41:17 -0600

    tasks

---
+++ folder/app/assets/javascripts/tasks_ctrl.js
@@ -0,0 +1,10 @@
+(function () {
+  "use strict";
+
+  angular.module("app").controller("taskCtrl", function($scope) {
+
+    $scope.message = "Hello!";
+    $scope.task1 = "Wash the dishes";
+    
+  });
+})();
