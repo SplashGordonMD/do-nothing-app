408c499595646afbe502f8a76d12505692bf5011
Casey Platt <kse0824@gmail.com>
2016-12-19 19:11:07 -0600

    introduction

--- js-callback-app/config/routes.rb
+++ js-callback-app/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ #  For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ #  function changeColors() {
+ #  	setTimeout(function() {
+ #  	document.getElementById("first").style["color"] = "red"
+ #  	}, 2000);
+ #  	documents.getElementById("first").style["color"] = "red";
+ #  	document.getElementById("second").style["color"] = "blue";
+	# }
+  get '/' => 'pages#home'
 end
