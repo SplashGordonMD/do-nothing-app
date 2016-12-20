81992cf18a8609388ba2822ef0875a22edbf334f
Casey Platt <kse0824@gmail.com>
2016-12-19 21:06:21 -0600

    more angular examples

--- folder/app/assets/javascripts/tasks_ctrl.js
+++ folder/app/assets/javascripts/tasks_ctrl.js
@@ -1,10 +1,17 @@
-(function () {
+/* global angular */
+
+(function() {
   "use strict";
 
   angular.module("app").controller("taskCtrl", function($scope) {
-
-    $scope.message = "Hello!";
-    $scope.task1 = "Wash the dishes";
+    $scope.myMessage = "Yo yo yo!";
+    $scope.myName = "Casey";
+    $scope.sum = 13 + 16;
+    $scope.tasks = ["Wash the dishes", "Go to Bed", "Say Good night"];
     
-  });
+    $scope.addTask = function(inputText) {
+     $scope.tasks.push(inputText);
+    };
+    });
+
 })();
