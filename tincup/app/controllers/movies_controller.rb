1f77e8916d3084d622d2ebdaf1c6e70879472a57
Casey Platt <kse0824@gmail.com>
2016-12-26 09:09:25 -0600

    latest

--- tincup/app/controllers/movies_controller.rb
+++ tincup/app/controllers/movies_controller.rb
@@ -12,7 +12,8 @@ class MoviesController < ApplicationController
 	def create
 		movie = Movie.new(name: params[:name], genera: params[:genera])
 		movie.save
-		render "create.html.erb"
+		#render "create.html.erb"
+		redirect_to '/movies/#{movies}'
 	end
 	
 	def show
@@ -23,20 +24,25 @@ class MoviesController < ApplicationController
 	def edit
 		@movie = Movie.find_by(id: params[:id])
 		render "edit.html.erb"
+		#redirect_to '/movies/#{movies:id}'
 	end
 	
 	def update
+		@movie_id = params[:id]
 		@movie = Movie.find_by(id: params[:id])
 		@movie.name = params[:name]
 		@movie.genera = params[:genera]
 		@movie.save 
-		render "update.html.erb"
+		#render "update.html.erb"
+		flash[:success] = "Movie successfully updated!"
+		redirect_to '/movies/#{movies:id}'
 	end
 	
 	def destroy
 		@movie = Movie.find_by(id: params[:id])
 		@movie.destroy
-		render "destroy.html.erb"
+		#render "destroy.html.erb"
+		redirect_to '/movies/#{movies:id}'
 	end
 	
 end
