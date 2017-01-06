212bfa67eb07ff9f734e7d04bbba6ca9ffeb8b40
Casey Platt <kse0824@gmail.com>
2017-01-05 19:05:06 -0600

    test

--- test-todo-app/spec/models/list_spec.rb
+++ test-todo-app/spec/models/list_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#complete_all_tasks!' do
+    it 'should mark all tasks from the list as complete' do
+      list = List.create(name: "Chores")
+      task1 = Task.create(complete: false, list_id: list.id)
+      task2 = Task.create(complete: false, list_id: list.id)
+      list.complete_all_tasks!
+      list.tasks.each do |task|
+        expect(task.complete).to eq(true)
+      end
+    end
+  end
 end
