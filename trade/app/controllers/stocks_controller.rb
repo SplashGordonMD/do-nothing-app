2be805eb48f40a5d6e1c004808a37bb43b768a96
Casey Platt <kse0824@gmail.com>
2017-08-18 16:20:12 -0500

    review API Ex

--- trade/app/controllers/stocks_controller.rb
+++ trade/app/controllers/stocks_controller.rb
@@ -2,35 +2,44 @@ class StocksController < ApplicationController
   def index
      @stocks = Stock.all
      render 'index.html.erb'
-    end
+  end
+
   def new
      render 'new.html.erb'
-    end
+  end
+
   def create
      stock = Stock.new(name: params[:name], price: params[:price])
      stock.save
-     render 'create.html.erb'
-    end
+     redirect_to "/stocks/#{stock.id}"
+  end
+
   def show
+     stock_id = params[:id]
      @stock = Stock.find_by(id: params[:id])
      render 'show.html.erb'
-    end
+  end
+
   def edit
-      @stock = Stock.find_by(id: params[:id])
+     stock_id = params[:id]
+     @stock = Stock.find_by(id: params[:id])
      render 'edit.html.erb'
-    end
+  end
+  
   def update
+    stock_id = params[:id]
     @stock = Stock.find_by(id: params[:id])
     @stock.name = params[:name]
     @stock.price = params[:price]
     @stock.save
-    render 'update.html.erb'
-    end
+    flash[:success] = "Stock successfully updated"
+    redirect_to "/stocks/#{@stock.id}"
+  end
+
   def destroy
+    stock_id = params[:id]
     @stock = Stock.find_by(id: params[:id])
     @stock.destroy 
-     render 'destroy.html.erb'
-    end
-
-
+    redirect_to "/stocks"
+  end
 end
