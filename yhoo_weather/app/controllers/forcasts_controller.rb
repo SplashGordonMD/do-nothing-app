d5d61b93505e3580ed9709e8c9cb2db6b5eec7ad
Casey Platt <kse0824@gmail.com>
2017-08-19 08:19:46 -0500

    organizing raw API data

---
+++ yhoo_weather/app/controllers/forcasts_controller.rb
@@ -0,0 +1,8 @@
+class ForcastsController < ApplicationController
+  def index
+    @data = Unirest.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22chicago%2C%20il%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").body
+    @temperature = @data["query"]["results"]["channel"]["item"]["condition"]["temp"]
+    @forcasts = @data["query"]["results"]["channel"]["item"]["forecast"]
+    render 'index.html.erb'
+  end
+end
