e781ec42e43f35767b9871052315e3e447371096
Casey Platt <kse0824@gmail.com>
2017-09-12 17:36:14 -0500

    better API

--- weather_from_yahoo/config/routes.rb
+++ weather_from_yahoo/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/weather_from_yahoo' => 'weather#index'
 end
